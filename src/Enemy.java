a212e11c9335523d304f9137471bbaa270ae66c2
Mojens <moha135h@stud.kea.dk>
2021-10-26 15:15:10 +0200

    Lavet enemy class

---
+++ src/Enemy.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Enemy {
+  private int health;
+  private String name;
+  private String weapon;
+
+
+  public Enemy(String name, int health,String weapon){
+    this.name = name;
+    this.health = health;
+    this.weapon = weapon;
+
+  }
+}
